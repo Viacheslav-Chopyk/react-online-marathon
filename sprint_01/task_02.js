d5f8817050c58715f096ff4bf30231e8e228d6ce
user <if040489cvv@gmail.com>
2020-12-20 12:42:18 +0200

    new

---
+++ sprint_01/task_02.js
@@ -0,0 +1,23 @@
+// Write a function combineArray(arr1, arr2), which takes 2 arrays, 
+//and returns a new array consisting only of numeric elements of arrays arr1 and arr2.
+// Function example:
+// combineArray([12, "User01", 22, true, -8], ["Index", 6, null, 15]));  // [12, 22, -8, 6, 15]
+const arr1 = [12, "user01", 22, true, -8];
+const arr2 = ["Index", 6, null, 15];
+function combineArray(arr1, arr2) {
+    let arr3 = [];
+    for (let i = 0; i < arr1.length; i++) {
+        if (typeof arr1[i] === 'number') {
+            arr3.push(arr1[i]);
+        }
+    }
+    for (let i = 0; i < arr2.length; i++) {
+        if (typeof arr2[i] === 'number') {
+            arr3.push(arr2[i]);
+        }
+
+    }
+    return arr3;
+}
+
+combineArray(arr1, arr2);
---
+++ sprint_01/task_03.js
@@ -0,0 +1,22 @@
+// Implement the longestLogin(loginList) function, which takes an array of user logins loginList  and returns the longest login. 
+//If the logins of the same length are the longest in the array, the login element with the largest index is returned. 
+//Tip: You can use the reduce() method to solve the task.
+// Function examples:
+// longestLogin(["serg22", "tester_2", "Prokopenko", "guest"]);   //  Prokopenko
+// longestLogin(["user1", "user2", "333", "user4", "aa"]);   //  user4
+
+function longestLogin(loginList) {
+    const result = loginList.reduce((accumulator, item, index, array) => {
+        if (accumulator.length < item.length) {
+            return index, item;
+        }
+        else if (item > accumulator[1]) {
+            return index, item;
+        }
+        else {
+            return accumulator;
+        }
+    });
+    return result;
+}
+longestLogin(loginList);
