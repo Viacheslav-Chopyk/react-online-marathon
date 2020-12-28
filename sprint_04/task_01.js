3f80e6d5e1c555c59aa19fc19fced57f354ca640
user <if040489cvv@gmail.com>
2020-12-28 15:24:50 +0200

    new

---
+++ sprint_04/task_01.js
@@ -0,0 +1,6 @@
+// Implement the getPromise(delay, message) function, which takes an integer number delay(between 0 and 2000) and
+// string message and returns a Promise that waits for specified amount of time(using delay argument) and resolves with the message.
+
+function getPromise(delay, message) {
+    // your code
+}
---
+++ sprint_04/task_02.js
@@ -0,0 +1,6 @@
+// Write an add(x, y) function that takes two arguments x and y.The function should return a Promise that
+// resolves with the sum of the two arguments if they are Numbers, or rejects with the message "Error!" otherwise.
+
+function add(x, y) {
+    // your code
+}
